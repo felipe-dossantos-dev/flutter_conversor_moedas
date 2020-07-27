3112090bcff6825188feac9d657fd154816f6bb5
felipe.garcia <brhbs0482@ambev.com.br>
2020-07-27 19:22:34 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_conversor_moedas
+
+Projeto 3 do curso de flutter
---
+++ android/app/src/main/kotlin/br/com/flutter_conversor_moedas/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.flutter_conversor_moedas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
